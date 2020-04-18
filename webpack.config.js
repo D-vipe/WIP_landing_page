44fb05cb501adb4cb5784b04bb0ae8e58247e522
d-vipe <ase@fastsol.ru>
2020-04-18 18:03:22 +0300

    gitignore

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+
+module.exports = {
+	entry: './src/index.js',
+	devServer: {
+		contentBase: './dist',
+	},
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					'file-loader?name=css/[name].css',
+					'extract-loader',
+					'css-loader',
+					{
+					  loader: 'resolve-url-loader',
+					  options: {
+					  	sourceMap: true,
+          				sourceMapContents: false
+					  }
+					},
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/,
+				use: [
+					'file-loader?name=img/[name].[ext]'
+				],
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					'file-loader?name=fonts/[name].[ext]'
+				],
+			},
+		]
+	},
+}

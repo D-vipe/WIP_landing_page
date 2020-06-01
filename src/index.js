9ea02e62f4ac671e52418751046afd152a66d515
d-vipe <ase@fastsol.ru>
2020-06-01 09:13:25 +0300

    input mask added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/style.sass'
 import './js/owl.carousel.min.js'
-import './js/main.js';
-import './js/animations';
+import './js/main.js'
+import './js/animations'
 
 /**
  * process all general to make webpack load them
--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,42 @@
+import Inputmask from "inputmask";
+
 $(document).ready(() => {
-    $('#clientSlider').owlCarousel({
-      loop: false,
-      nav: true,
-      dots: false,
-      items: 3,
-      stagePadding: 50,
-      responsive: {
-        320: {
-          items: 1,
-          stagePadding: 0
-        },
-        768: {
-          items: 1,
-          stagePadding: 124
-        },
-        1024: {
-          items: 2,
-          stagePadding: 82
-        },
-        1440: {
-          items: 2,
-          stagePadding: 60
-        },
-        1920: {
-          items: 3,
-          stagePadding: 50
-        }
+  $('#clientSlider').owlCarousel({
+    loop: false,
+    nav: true,
+    dots: false,
+    items: 3,
+    stagePadding: 50,
+    responsive: {
+      320: {
+        items: 1,
+        stagePadding: 0
+      },
+      768: {
+        items: 1,
+        stagePadding: 124
+      },
+      1024: {
+        items: 2,
+        stagePadding: 82
+      },
+      1440: {
+        items: 2,
+        stagePadding: 60
+      },
+      1920: {
+        items: 3,
+        stagePadding: 50
       }
-    });
+    }
+  });
 
   $('#experienceSlider').owlCarousel({
     loop: false,
     nav: true,
     dots: false,
     stagePadding: 150,
-    responsiveClass:true,
+    responsiveClass: true,
     responsive: {
       320: {
         items: 1,
@@ -55,87 +57,78 @@ $(document).ready(() => {
     }
   });
 
-   function checkTelephone(telValue) {
-     let exp = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/,
-         errorBlock = $('.error-message'),
-         telValid = false,
-         errorType = 0;
+  function checkTelephone(telValue) {
+    let exp = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/,
+      errorBlock = $('.error-message'),
+      telValid = false,
+      errorType = 0;
 
-     errorBlock.text('');
-     telValid = telValue !== '';
-     if (!telValid) {
-       errorType = 1;
-     } else {
-       telValid = telValue.search(exp) !== -1;
-       if (!telValid) {
-         errorType = 2;
-       }
-     }
+    errorBlock.text('');
+    telValid = telValue !== '';
+    if (!telValid) {
+      errorType = 1;
+    } else {
+      telValid = telValue.search(exp) !== -1;
+      if (!telValid) {
+        errorType = 2;
+      }
+    }
 
-     if (!telValid) {
-       //show error message
-       $('input[name=tel]').addClass('error');
-       let error_text = errorType === 1 ? 'введите номер телефона' : 'неправильный формат';
-       errorBlock.text(error_text).fadeIn();
-       // $('#pageForm').find('button[type=submit]').attr('disabled', true);
-     } else {
-       //remove error message if any
-       $('input[name=tel]').removeClass('error');
-       errorBlock.fadeOut();
-       // $('#pageForm').find('button[type=submit]').attr('disabled', false);
-     }
+    if (!telValid) {
+      //show error message
+      $('input[name=tel]').addClass('error');
+      let error_text = errorType === 1 ? 'введите номер телефона' : 'неправильный формат';
+      errorBlock.text(error_text).fadeIn();
+      // $('#pageForm').find('button[type=submit]').attr('disabled', true);
+    } else {
+      //remove error message if any
+      $('input[name=tel]').removeClass('error');
+      errorBlock.fadeOut();
+      // $('#pageForm').find('button[type=submit]').attr('disabled', false);
+    }
 
-     return telValid;
-   }
+    return telValid;
+  }
 
-   function confirmFormSubmit() {
-     const formBlock = $('#pageForm'),
-       leftBlock = $('.form-help-text p'),
-       initialMessage = 'Возникли вопросы или хотите оценить стоимость проекта? Оставьте заявку и наш' +
-         ' менеджер с вами свяжется.',
-       newMessage = 'Спасибо! В течении дня с вами свяжется наш менеджер для уточнения деталей проекта.';
+  function confirmFormSubmit() {
+    const formBlock = $('#pageForm'),
+      leftBlock = $('.form-help-text p'),
+      initialMessage = 'Возникли вопросы или хотите оценить стоимость проекта? Оставьте заявку и наш' +
+        ' менеджер с вами свяжется.',
+      newMessage = 'Спасибо! В течении дня с вами свяжется наш менеджер для уточнения деталей проекта.';
 
-     formBlock.fadeOut();
-     formBlock[0].reset();
-     leftBlock.text(newMessage);
+    formBlock.fadeOut();
+    formBlock[0].reset();
+    leftBlock.text(newMessage);
 
-     setTimeout(() => {
-       formBlock.fadeIn();
-       leftBlock.text(initialMessage);
-     }, 3500);
-   }
+    setTimeout(() => {
+      formBlock.fadeIn();
+      leftBlock.text(initialMessage);
+    }, 3500);
+  }
 
 
-   $('input[name=name]').on('change', function() {
-     let element = $(this);
-     if (element.val().length > 0) {
-       console.log(element.val() + ' Возникли вопросы или хотите оценить стоимость' +
-         ' проекта? Оставьте заявку и наш менеджер с вами свяжется.');
-        $('textarea[name=comment]').val(element.val() + ' Возникли вопросы или хотите оценить стоимость' +
-       ' проекта? Оставьте заявку и наш менеджер с вами свяжется.')
-     } else {
-       $('textarea[name=comment]').val('Сообщение');
-     }
-   });
+  // tel input mask
+  Inputmask({"mask": "+7(999) 999-99-99"}).mask($('input[name=tel]'));
 
-   $('input[name=tel]').on('change', function(){
-     let  tel = $(this).val();
-     checkTelephone(tel);
-   });
+  $('input[name=tel]').on('change', function () {
+    let tel = $(this).val();
+    checkTelephone(tel);
+  });
 
-   $('#pageForm').submit(function(e) {
-     e.preventDefault();
-     //validate tel input
-     let telInput = $('input[type=tel]'),
-         formValid = checkTelephone(telInput.val());
+  $('#pageForm').submit(function (e) {
+    e.preventDefault();
+    //validate tel input
+    let telInput = $('input[type=tel]'),
+      formValid = checkTelephone(telInput.val());
 
-     if (formValid) {
-       confirmFormSubmit();
-     }
-   });
+    if (formValid) {
+      confirmFormSubmit();
+    }
+  });
 
-   $('.button_callback').click(function () {
-     console.log(123);
-   });
+  $('.button_callback').click(function () {
+    console.log(123);
+  });
 
 });
